609c43ef52d84b8f1b78cc88ab265a4c65128687
Paul Gebeline <psgebeli@go.olemiss.edu>
2023-03-16 17:19:39 -0500

    initial commit

---
+++ cpp/headers/word.h
@@ -0,0 +1,22 @@
+#ifndef WORD_H
+#define WORD_H
+
+// cpp libraries
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <fstream>
+using std::ifstream;
+
+#include <vector>
+using std::vector;
+
+#include <cstdlib>
+using std::srand;
+using std::rand;
+
+#include <ctime>
+using std::time;
+
